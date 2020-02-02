644b10b7841c4a40308f56bd7190387d9e7665f8
jenchieh <jcs090218@gmail.com>
2020-02-02 22:56:06 +0800

    Add core.

---
+++ manage-minor-mode-plus.el
@@ -0,0 +1,38 @@
+;;; manage-minor-mode-plus.el --- Manage minor-modes in table  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-02-02 22:50:10
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Manage minor-modes in table
+;; Keyword: tools minor-mode manage
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3") (manage-minor-mode "1.1"))
+;; URL: https://github.com/jcs090218/manage-minor-mode-plus
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Manage minor-modes in table.
+;;
+
+;;; Code:
+
+
+
+(provide 'manage-minor-mode-plus)
+;;; manage-minor-mode-plus.el ends here
